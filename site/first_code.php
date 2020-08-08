414cdc2298f2c6dbf5aa526ccb9352977991a4eb
unknown <vikramsuresh27@gmail.com>
2020-08-08 13:52:31 +0530

    php test

---
+++ site/first_code.php
@@ -0,0 +1,9 @@
+<html>
+<head><h2>This is the response page</h2></head>
+<?php
+$uname=$_POST['user'];
+$uid=$_POST['id'];
+echo "<h3>".$uname."</h3>"."<br>";
+echo "<h3>".$uid."</h3>";
+?>
+</html>
